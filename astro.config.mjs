274d3218203c832aa9f25e5b5a1792cf522b9e4b
Robespierre Triviño <robtrivi@espol.edu.ec>
2026-03-10 09:45:56 -0500

    chore: agregar plugin de Icons a la configuración de Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      Icons({
+        compiler: 'astro'
+      })
+    ]
   }
 });
